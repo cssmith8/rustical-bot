d8f927069fa01c41144c820a1bd8348a42bf1223
cssmith8 <cohens@smu.edu>
2025-10-13 09:04:35 -0500

    advanced Rust updates

--- src/commands/translate_list.rs
+++ src/commands/translate_list.rs
@@ -1,6 +1,9 @@
-use crate::types::types::{Context, Error};
-use crate::{types::translation::Translation, utils::translations::load_translations};
-
+use crate::{
+    types::{
+        types::{Context, Error},
+    },
+    utils::translations::load_translations,
+};
 use anyhow::Result;
 
 #[poise::command(slash_command, prefix_command)]
@@ -8,10 +11,7 @@ pub async fn translate_list(ctx: Context<'_>) -> Result<(), Error> {
     let all = load_translations().unwrap_or_default();
     let mut message = String::new();
     for translation in all {
-        message.push_str(&translation.abbreviation);
-        message.push_str(": ");
-        message.push_str(&translation.definition);
-        message.push('\n');
+        message.push_str(&format!("{}: {}\n", translation.abbreviation, translation.definition));
     }
 
     ctx.say(message).await?;
