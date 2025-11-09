d548e63f2946c0c5a88c6ac565cd39832ae2984a
cssmith8 <cohens@smu.edu>
2025-11-08 19:18:55 -0600

    db

--- src/utils/db.rs
+++ src/utils/db.rs
@@ -1,6 +1,10 @@
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
 
 pub fn create_or_open_db(path: String) -> PickleDb {
+    //pickle db
+    //2
+    //3
+    // third
     let opendb = match PickleDb::load(
         path.clone(),
         PickleDbDumpPolicy::AutoDump,
